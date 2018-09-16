e3ff8f82f55d67ffb82378e7cfb10f5ae61d0646
Your Name <you@example.com>
2018-09-16 19:36:04 -0400

    save

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,59 +4,130 @@ var losses = 0;
 var wins = 0;
 
 //Computer generates random number between 19 - 120
-function randomNumberGen(){
-    var randomNumber = [Math.floor(Math.random() * 101) + 19];
-}
-
     //display random number
+//function randomNumberGen(){
+    var randomNumber = Math.floor((Math.random() * 101) + 19);
+    console.log(randomNumber);
+    document.getElementById("randomNumberBox").innerHTML = randomNumber;
 
+   
+//}
 
+    
+document.getElementById("totalScore").innerHTML = totalScoreCounter;
 
-
-
-//Generate random number between 1 & 12 for each button
 function buttonsValueGen(){
-    var button1Value = [Math.floor(Math.random() * 11) + 1];
-    var button2Value = [Math.floor(Math.random() * 11) + 1];
-    var button3Value = [Math.floor(Math.random() * 11) + 1];
-    var button4Value = [Math.floor(Math.random() * 11) + 1];
-}
+    var button1Value = Math.floor((Math.random() * 11) + 1);
+    var button2Value = Math.floor((Math.random() * 11) + 1);
+    var button3Value = Math.floor((Math.random() * 11) + 1);
+    var button4Value = Math.floor((Math.random() * 11) + 1);
+    console.log(button1Value);
 
-//.onclick response for each button
-$("#button1").click(function(){
-    button1Value += totalScoreCounter;
+
+$(".button").click(function(){
+    if(id = "button1"){
+        totalScoreCounter = button1Value + totalScoreCounter;
+    document.getElementById("totalScore").innerHTML = totalScoreCounter;
+    }
+
+    if(id = "button2"){
+        totalScoreCounter = button2Value + totalScoreCounter;
+    document.getElementById("totalScore").innerHTML = totalScoreCounter;
+    }
+
+    if(id = "button3"){
+        totalScoreCounter = button3Value + totalScoreCounter;
+    document.getElementById("totalScore").innerHTML = totalScoreCounter;
+    }
+
+    if(id = "button4"){
+        totalScoreCounter = button4Value + totalScoreCounter;
+    document.getElementById("totalScore").innerHTML = totalScoreCounter;
+    }
+
+    if(totalScoreCounter > randomNumber){
+        losses++;
+        randomNumber = Math.floor((Math.random() * 101) + 19);
+    console.log(randomNumber);
+    document.getElementById("randomNumberBox").innerHTML = randomNumber;
+        buttonsValueGen();
+        totalScoreCounter = 0;
+    }
+
+    else if(totalScoreCounter == randomNumber){
+        wins++;
+        randomNumber = Math.floor((Math.random() * 101) + 19);
+    console.log(randomNumber);
+    document.getElementById("randomNumberBox").innerHTML = randomNumber;
+        buttonsValueGen();
+        totalScoreCounter = 0;
+    }
 })
+}
+//Generate random number between 1 & 12 for each button
+
+    /*$("#button1").click(function(){
+    totalScoreCounter = button1Value + totalScoreCounter;
+    document.getElementById("totalScore").innerHTML = totalScoreCounter;
+    console.log(totalScoreCounter);
+     if(totalScoreCounter > randomNumber){
+        losses++;
+        randomNumber = Math.floor((Math.random() * 101) + 19);
+    console.log(randomNumber);
+    document.getElementById("randomNumberBox").innerHTML = randomNumber;
+        buttonsValueGen();
+        totalScoreCounter = 0;
+    }
+
+    else if(totalScoreCounter == randomNumber){
+        wins++;
+        randomNumber = Math.floor((Math.random() * 101) + 19);
+    console.log(randomNumber);
+    document.getElementById("randomNumberBox").innerHTML = randomNumber;
+        buttonsValueGen();
+        totalScoreCounter = 0;
+    }
+ })  
 $("#button2").click(function(){
-    button2Value += totalScoreCounter;
+    totalScoreCounter = button2Value + totalScoreCounter;
+    document.getElementById("totalScore").innerHTML = totalScoreCounter;
+    console.log(totalScoreCounter);
 })
 $("#button3").click(function(){
-    button3Value += totalScoreCounter;
+    totalScoreCounter = button3Value + totalScoreCounter;
+    document.getElementById("totalScore").innerHTML = totalScoreCounter;
+    console.log(totalScoreCounter);
 })
 $("#button4").click(function(){
-    button4Value += totalScoreCounter;
+    totalScoreCounter = button4Value + totalScoreCounter;
+    document.getElementById("totalScore").innerHTML = totalScoreCounter;
+    console.log(totalScoreCounter);
 })
+    
+}*/
+
+ 
+
+//.onclick response for each button
     //add random button value to total score counter
 
+
 //if total score counter > random number
-if(totalScoreCounter > randomNumber){
-    losses++;
-    randomNumberGen();
-    buttonsValueGen();
-    totalScoreCounter = 0;
-}
     //incrememnt losses counter and restart game:
         //generate new random number
         //generate new random value
         //reset total score counter to 0
 
+    
+
 //if total score counter == random number
-else if(totalScoreCounter == randomNumber){
-    wins++;
-    randomNumberGen();
-    buttonsValueGen();
-    totalScoreCounter = 0;
-}
     //increment wins counter and restart game:
         //generate new random number
         //generate new random value
         //reset total score counter to 0
+
+
+
+       // randomNumberGen();
+
+        buttonsValueGen();
